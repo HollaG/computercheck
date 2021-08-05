e7d6d886211bd8e5663032c6bc6b9360a17d4ef6
Holla <marcussoh38@gmail.com>
2021-08-05 15:40:27 +0800

    scripts

--- scripts/specifications.js
+++ scripts/specifications.js
@@ -1,149 +1,29 @@
 const fs = require("fs-extra")
-// const mysql = require("mysql2/promise")
-// const puppeteer = require("puppeteer")
+const mysql = require("mysql2/promise")
+const puppeteer = require("puppeteer")
 
-// const db = require("../configuration/database.json")
-// const pool = mysql.createPool(db)
+const db = require("../configuration/database.json")
+const pool = mysql.createPool(db)
 // const { Cluster } = require('puppeteer-cluster');
 
-// var startTime = new Date().getTime()
-// console.log("Started script at " + startTime)
+var startTime = new Date().getTime()
+console.log("Started script at " + startTime)
 
     
     ; (async () => {
-//         let conn = null
-//         try {
+        let conn = null
+        try {
 
-//             const cluster = await Cluster.launch({
-//                 concurrency: Cluster.CONCURRENCY_PAGE,
-//                 maxConcurrency: 4
-//             })
+            const cluster = await Cluster.launch({
+                concurrency: Cluster.CONCURRENCY_PAGE,
+                maxConcurrency: 4
+            })
             const dictionary = await fs.readJSON(`${process.cwd()}/data/dictionary.json`)
             const extras = await fs.readJSON(`${process.cwd()}/data/extras.json`)
-//             const main = await fs.readJSON(`${process.cwd()}/data/main.json`)
-//             const unidentified = main["unidentified"]
-
-//             const browser = await puppeteer.launch({ headless: false });
-
-//             conn = await pool.getConnection()
-//             let stores = await conn.query(`SELECT DISTINCT location FROM data ORDER BY location ASC`)
-//             stores = stores[0] 
-
-//             for (let i = 0; i < unidentified.length; i++) {
-//                 let model = unidentified[i]
-//                 console.log(model.location)
-
-
-//                 switch (model.location) { 
-//                     case "Harvey Norman":
-//                         break // TODO
-//                         console.log(model.link)
-//                         const hnPage = await browser.newPage()
-//                         await hnPage.goto(model.link, {waitUntil: "networkidle2"})
-//                         let model_ID = await hnPage.$eval("#content_features > div > table:nth-child(3)", table => {
-//                             let headings = Array.from(table.querySelectorAll('tr > th'))
-//                             let index;
-//                             headings.forEach(heading => { 
-//                                 if (heading.innerText.toUpperCase() == "MODEL") index = headings.indexOf(heading) + 1
-//                             })  
-//                             return table.querySelector(`tr:nth-child(${index}) > td`).innerText.toUpperCase().trim()
-
-//                         })
-//                         console.log(model_ID)
-//                         if (!model_ID) searchMfgWebsite(model) 
-//                         else model.model_ID = model_ID
-                        
-//                         break;
-//                     case "Challenger":
-//                         // Scrape the site for possible ID linking to minisite
-//                         console.log("Scraping for " + model.name)
-
-                       
-                        
-//                         const cPage = await browser.newPage()
-//                         await cPage.goto(model.link, {waitUntil: "networkidle0"})
-                        
-//                         let modelName = await cPage.evaluate(() => {
-//                             let elem = document.querySelector(".flix-model-title")
-//                             if (!elem) { 
-//                                 return ""
-//                             } else {
-//                                 return elem.innerText.trim().toUpperCase()
-//                             }
-//                         })
-//                         console.log("Model: " + modelName)    
-
-//                         let isNotModel = false
-//                         for (word of modelName.split(" ")) { 
-//                             if (dictionary[word.toLowerCase()] || extras[word.toLowerCase]) { 
-//                                 isNotModel = true
-//                                 break;
-//                             }
-//                         }
-                        
-//                         await cPage.close()
-                        
-//                         if (isNotModel) { 
-//                             model.model_ID = ""
-//                             await searchMfgWebsite(model)
-//                         } else { 
-//                             model.model_ID = modelName
-//                         }
-                        
-
-                        
-                        
-                    
-//                         break;
-//                     // default: await searchMfgWebsite(model)
-//                 }
-//                 async function searchMfgWebsite(model) {
-
-                    
-//                     switch (model.brand) {
-
-//                         case "ACER": 
-//                             break // TODO
-//                             const aPage = await browser.newPage()
-
-//                         case "AVITA": 
-//                             const AVITA_MODELS = ["ADMIROR", 'LIBER', 'PURA', 'ESSENTIAL', 'MAGUS']
-                            
-//                             // Identify the avita model from the model name
-//                             let modelArr = model.name.toUpperCase().trim().split(" ")
-//                             let avita_model = ""
-//                             for (var i = 0; i < modelArr.length; i++) { 
-//                                 if (AVITA_MODELS.includes(modelArr[i])) {
-//                                     avita_model = modelArr[i]
-//                                     break;
-//                                 }
-//                             }
-//                             console.log(avita_model, " model model model")
-//                             model.model_ID = avita_model // Until we find a proper way to find the AVITA model number, this will have to do
-//                             break;
-
-//                         case "LENOVO": 
-//                             break // TODO
-//                             const cPage = await browser.newPage()
-//                             await cPage.goto(`https://www.lenovo.com/sg/en/search?text=${model.name}`, {waitUntil: 'networkidle2'})
-//                             let productPage = await cPage.$eval('.product-card-link', elem => elem.getAttribute("href"))
-//                             await cPage.goto(productPage, {waitUntil: 'networkidle2'})
-//                             let model_ID = await cPage.$eval(".partNumber", elem => {
-//                                 let text = elem.innerText
-//                                 return elem.innerText.replace("Part Number:").trim().toUpperCase()
-//                             })
-//                             model.model_ID = model_ID
-//                             cPage.close()
-//                             break;
-//                         default: return
-//                     }
-//                 }
-//             }
-//             await fs.writeFile(`${process.cwd()}/data/temp.json`, JSON.stringify(unidentified))
-//             await browser.close()
-//         } catch (e) {
-//             console.log(e)
-//         }
+            
+        } catch (e) {
+            console.log(e)
+        }
 
 
     console.log(cleaner("MICROSOFT SURFACE LAPTOP 4 5PB-00018 13IN AMD RYZEN 5 8GB 256SSD WIN10"))
